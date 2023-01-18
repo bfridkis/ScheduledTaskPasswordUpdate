f1101400660308b8f7f2f7a37158309c0b420b1e
Benjamin Fridkis <32713413+bfridkis@users.noreply.github.com>
2023-01-18 17:51:31 -0600

    Delete ScheduledTaskPasswordUpdate-LocalOnly.ps1

--- ScheduledTaskPasswordUpdate-LocalOnly.ps1
+++
@@ -1,5 +0,0 @@
-﻿$TaskCredential = Get-Credential
-Get-ScheduledTask | Where-Object { $_.Principal.UserId -eq $TaskCredential.UserName } | Set-ScheduledTask -User $TaskCredential.UserName -Password $TaskCredential.GetNetworkCredential().Password
-
-# Reference
-# https://serverfault.com/questions/1043188/update-task-scheduler-job-password-on-multiple-machines
