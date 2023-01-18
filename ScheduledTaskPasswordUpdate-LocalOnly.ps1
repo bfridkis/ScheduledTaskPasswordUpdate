2c073ea8f88b61c8741f0e705048810e64de33e6
Benjamin Fridkis <32713413+bfridkis@users.noreply.github.com>
2023-01-18 17:51:49 -0600

    Add files via upload

---
+++ ScheduledTaskPasswordUpdate-LocalOnly.ps1
@@ -0,0 +1,5 @@
+﻿$TaskCredential = Get-Credential -Message "Input username and new password for scheduled task(s) update."
+Get-ScheduledTask | Where-Object { $_.Principal.UserId -eq $TaskCredential.UserName } | Set-ScheduledTask -User $TaskCredential.UserName -Password $TaskCredential.GetNetworkCredential().Password
+
+# Reference
+# https://serverfault.com/questions/1043188/update-task-scheduler-job-password-on-multiple-machines
